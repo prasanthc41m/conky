c13f2d56a78b71b9bbae2056000af78a68271fb5
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-04-04 09:14:03 +0530

    Update conky-install.sh

--- conky-install.sh
+++ conky-install.sh
@@ -3,7 +3,7 @@
 apt install conky -y
 cd /tmp/ && rm -rf conky*
 git clone https://github.com/prasanthc41m/conky
-cp conky/* /etc/conky/conky/
+cp conky/* /etc/conky/
 conky > /dev/null 2>&1
 exit
 line="@reboot conky"
