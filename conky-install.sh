c70c39fe530e7acf7c6841a7079d42efc076abcb
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-04-04 10:12:01 +0530

    Update conky-install.sh

--- conky-install.sh
+++ conky-install.sh
@@ -5,8 +5,8 @@ cd /tmp/ && rm -rf conky/
 git clone https://github.com/prasanthc41m/conky
 sudo mv -f conky /etc/conky
 sudo pkill conky
-conky > /dev/null 2>&1
 exit
+conky > /dev/null 2>&1
 line="@reboot conky"
 (crontab -u $(whoami) -l; echo "$line" ) | crontab -u $(whoami) -
 echo 'Conky is running...'
