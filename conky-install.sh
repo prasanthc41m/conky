253e2fa49c4efdd14387b6611e374bc47788f4d8
Prasanth c41m <58906808+prasanthc41m@users.noreply.github.com>
2022-05-02 10:09:22 +0530

    Update conky-install.sh

--- conky-install.sh
+++ conky-install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #Conky from c41m
-apt install conky -y
+# install conky on debian based os with requirements
+sudo apt install conky-all git fonts-font-awesome grep libx11-dev gawk lm-sensors smartmontools -y
 cd /tmp/ && rm -rf conky/
 git clone https://github.com/prasanthc41m/conky
 sudo cp -rf conky /etc/
+# make executable
+sudo chmod +x /etc/conky/*
 sudo pkill conky
 conky > /dev/null 2>&1
 line="@reboot conky"
