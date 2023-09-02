530563611e746b39d0c8e0100bf36f19ec993bb9
Prasanth K <58906808+prasanthc41m@users.noreply.github.com>
2023-09-02 16:54:21 +0530

    Update conky-install.sh

--- conky-install.sh
+++ conky-install.sh
@@ -19,25 +19,6 @@ result2="s/wlan/$wlan/g"
 #echo -e debug: ${Y}$result1 $result2${NOCOL}
 grep -rl 'eth' /etc/conky/conky.conf | xargs sed -i "$result1"  
 grep -rl 'wlan' /etc/conky/conky.conf | xargs sed -i "$result2"
-#GPU configuration
-while true
-do
-read -r -p $'\033[0;33m "Do you have Nvidia GPU? [Y/n]"'   input
-
-      case $input in
-            [yY][eE][sS]|[yY])
-                  echo "Yes" 
-                  break
-                  ;;
-            [nN][oO]|[nN])
-                  echo "No" ; sudo sed -i '/^${color2}${exec nvidia-smi --query-gpu=gpu_name --format=csv,noheader,nounits}$color ${goto 210} ${exec nvidia-smi | grep % | cut -c 61-63} % ${goto 270} ${exec nvidia-smi | grep % | cut -c 37-40} MB ${goto 340}${exec nvidia-smi | grep % | cut -c 21-23} W       ${color3}${nvidia temp}°C$/s/^/#/' /etc/conky/conky.conf ; sudo sed -i '/^${color2}AMD Radeon     ${color3}${font :size= 9}${lua_parse igputemp}°C$/s/^/#/' /etc/conky/conky.conf ; echo -e "${Y}""Done""${NOCOL}"
-                  break
-                  ;;
-            *)
-                  echo "Invalid input..."
-                  ;;
-      esac      
-done
 # make executable
 sudo chmod +x /etc/conky/*
 sudo pkill conky
